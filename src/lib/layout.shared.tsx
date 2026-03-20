6f90ed4e8cce3011c1aad5f3e7db1b603d224c56
Pedro Fernandes <pedrohasftb@gmail.com>
2026-03-20 01:28:46 -0300

    links and correct repo

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,9 +1,10 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import { ExternalLink } from "lucide-react";
 
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
   user: "houseofstake",
-  repo: "houseofstake.org",
+  repo: "hos-docs",
   branch: "main",
 };
 
@@ -13,5 +14,12 @@ export function baseOptions(): BaseLayoutProps {
       title: "House of Stake",
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
+    links: [
+      {
+        text: "Govern HoS",
+        url: "https://gov.houseofstake.org",
+        icon: <ExternalLink />,
+      },
+    ],
   };
 }
